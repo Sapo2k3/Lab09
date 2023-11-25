73acc2f55fe4207e519f3da82e21764b0103956a
Mattia Saponaro <mattia.saponaro2@studio.unibo.it>
2023-11-25 15:07:47 +0100

    In exercise 3 added the implementation of the io controller which writes on a file. The default file is output.txt located in the user home directory. The controller has 4 methods: 1) setCurrentFile sets the file where the controller will write on; 2) getCurrentFile returns the name of the current file; 3) getCurrentFilePath returns the path of the current file; 4) saveOnCurrentFile writes on the current File the String passed as input

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,53 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    private String PATH = System.getProperty("user.home")
+    + File.separator
+    + BadIOGUI.class.getSimpleName() + ".txt";
+    
+    private File currentFile = new File(PATH);
 
+    public void setFile(final File file){
+        if(file.exists()){
+            this.currentFile = file;
+        }
+        throw new NullPointerException("The file does not exist");        
+    }
+
+    public File getCurrentFile(){
+        if(currentFile.exists()){
+            return this.currentFile;
+        }
+        throw new NullPointerException("No file has been set");
+    }
+
+    public String getCurrentFilePath(){
+        if(currentFile.exists()){
+            return this.currentFile.toPath().toString();
+        }
+        throw new NullPointerException("No file has been set");
+    }
+
+    public void saveOnCurrentFile(final String content){
+        if(content.isBlank()){
+            throw new IllegalArgumentException("The string is blank.");
+        }
+        else if(this.currentFile.exists()){
+            try (PrintStream ps = new PrintStream(currentFile)){
+                ps.println(content);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            throw new NullPointerException("No file has been set");
+        }
+    }
 }
